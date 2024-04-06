f65f23e9aa0d60732f347f8c8a889e70eb7dd4e3
shuvamg <156459213+shuvamg@users.noreply.github.com>
2024-04-06 19:31:37 +0530

    Create differentiate99.f
    derivative of a given function using forward and central difference method and compare them with exact solution
    (Fortran 77 code)

---
+++ differentiate.f
@@ -0,0 +1,30 @@
+      program  derivative
+      implicit none
+      real :: value, v
+      print*, "          h   " , "       exact" , "          fd" ,
+     & "                    cd" , "            error_fd" ,
+     & "        error_cd"
+      value = v(0.3,0.1)
+      value= v(0.3,0.05)
+      value= v(0.3, 0.025)
+      end
+
+      function  v(x,h)
+      real :: h,x f,fd,cd, v, value, exact
+      fd= (f(x+h)- f(x))/h
+      cd= (f(x+h)- f(x-h)) / (2.0*h)
+      exact= (1-(x/2))*exp(-(x/2))
+      error_fd= abs(exact- fd)
+      error_cd= abs(exact- cd)
+      print*, h, exact, fd, cd, error_fd, error_cd
+      end
+
+
+
+      function f(x)
+      !implicit none
+      real :: x
+      f= x*exp(-(x/2))
+      return
+      stop
+      end
